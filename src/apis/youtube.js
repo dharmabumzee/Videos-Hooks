1f15786328aacf3cb23fecc0e4f0b15d0f3a761c
lukabelani <luka.belani@gmail.com>
2020-09-27 19:10:58 +0200

    all

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const KEY = your_youtube_api_key;
+const KEY = "AIzaSyBeBfSSIDT3xq4B-TL46nHjT8Ma0skll9Q";
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
