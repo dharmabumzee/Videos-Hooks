dac223a19c3b6842a3d9b7c9d57b9a7918e2e0fa
lukabelani <luka.belani@gmail.com>
2020-09-27 19:32:41 +0200

    fix

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const KEY = "AIzaSyBeBfSSIDT3xq4B-TL46nHjT8Ma0skll9Q";
+const KEY = "";
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
